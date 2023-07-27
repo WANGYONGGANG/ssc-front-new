cd0640ca1b7e0d4afe275d7fed35346ef48d3a6d
wangyonggang <135274859@qq.com>
2023-07-27 18:43:52 +0800

    update

--- ssc-frontend/src/components/rf-product-list/index.vue
+++ ssc-frontend/src/components/rf-product-list/index.vue
@@ -12,7 +12,12 @@
 					:hover-start-time="150"
 				>
 					<view class="rf-product-image-wrapper">
-						<image :src="item.picture" mode="widthFix" :preview="false" :class="[isList?'rf-product-list-img':'rf-product-img']" ></image>
+						<image
+							:src="item.picture"
+							mode="widthFix"
+							:preview="false"
+							:class="[isList ? 'rf-product-list-img' : 'rf-product-img']"
+						></image>
 						<text class="sketch in1line">{{ item.sketch }}</text>
 						<view class="triangle-wrapper">
 							<image class="triangle-tag" :src="item | filterTagName"></image>
@@ -22,8 +27,14 @@
 						<view class="rf-pro-tit">{{ item.name }}</view>
 						<view v-if="item">
 							<view class="rf-pro-price">
-								<text class="rf-sale-price" :class="'text-'+themeColor.name">{{ moneySymbol }}{{ item.price }}</text>
-								<text class="rf-factory-price" v-if="item.market_price > item.price">{{ moneySymbol }}{{ item.market_price }}</text>
+								<text class="rf-sale-price" :class="'text-' + themeColor.name"
+									>{{ moneySymbol }}{{ item.price }}</text
+								>
+								<text
+									class="rf-factory-price"
+									v-if="item.market_price > item.price"
+									>{{ moneySymbol }}{{ item.market_price }}</text
+								>
 							</view>
 						</view>
 					</view>
@@ -43,9 +54,22 @@
 					:hover-start-time="150"
 				>
 					<view class="rf-product-image-wrapper">
-						<image :src="item.picture" mode="widthFix" :preview="false" :class="[isList?'rf-product-list-img':'rf-product-img']" ></image>
+						<image
+							:src="item.picture"
+							mode="widthFix"
+							:preview="false"
+							:class="[isList ? 'rf-product-list-img' : 'rf-product-img']"
+						></image>
 						<text class="sketch in1line">{{ item.sketch }}</text>
-						<view class="triangle-wrapper" v-if="item.shipping_type === '1' || item.is_virtual === '1' || item.commissionRate || item.is_open_presell === '1'">
+						<view
+							class="triangle-wrapper"
+							v-if="
+								item.shipping_type === '1' ||
+								item.is_virtual === '1' ||
+								item.commissionRate ||
+								item.is_open_presell === '1'
+							"
+						>
 							<image class="triangle-tag" :src="item | filterTagName"></image>
 						</view>
 					</view>
@@ -53,138 +77,56 @@
 						<view class="rf-pro-tit">{{ item.name }}</view>
 						<view v-if="item">
 							<view class="rf-pro-price">
-								<text class="rf-sale-price" :class="'text-'+themeColor.name">{{ moneySymbol }}{{ item.price }}</text>
-								<text class="rf-factory-price" v-if="item.market_price > item.price">{{ moneySymbol }}{{ item.market_price }}</text>
+								<text class="rf-sale-price" :class="'text-' + themeColor.name"
+									>{{ moneySymbol }}{{ item.price }}</text
+								>
+								<text
+									class="rf-factory-price"
+									v-if="item.market_price > item.price"
+									>{{ moneySymbol }}{{ item.market_price }}</text
+								>
 							</view>
-							
 						</view>
 					</view>
 				</view>
 				<!--商品列表-->
 			</block>
 		</view>
-
 	</view>
 </template>
 <script>
-
-
-
-import $mAssetsPath from '@/config/assets.config';
-import { mapMutations } from 'vuex';
+import $mAssetsPath from "@/config/assets.config";
+import { mapMutations } from "vuex";
 export default {
-	name: 'rfProductList',
+	name: "rfProductList",
 	props: {
 		list: {
 			type: Array,
 			default() {
 				return [];
-			}
+			},
 		},
 		// 是否以列表展示 | 列表或大图
 		isList: {
 			type: Boolean,
-			default: false
+			default: false,
 		},
 		// 是否以列表展示 | 列表或大图
-		bottom: {
-			type: Number,
-			default: 0
-		}
+	
 	},
 	components: {
-		rfAttrContent
 	},
 	data() {
 		return {
-			specClass: 'none',
-      moneySymbol: this.moneySymbol,
-			productDetail: {}
+			
 		};
 	},
-	filters: {
-		filterTotalSales(val) {
-			if (val > 10000) {
-				val = `${(val / 10000).toFixed(2)}w`;
-			}
-			return val;
-		},
-		filterTagName(val) {
-			if (val.commissionRate) {
-				// 分销商品
-				return $mAssetsPath.distribution;
-			} else if (val.is_virtual === '1') {
-				// 虚拟产品
-				return $mAssetsPath.virtual;
-			} else if (val.shipping_type === '1') {
-				// 包邮产品
-				return $mAssetsPath.pinkage;
-			}
-		}
-	},
+
 	methods: {
 		// 跳转详情
 		navTo(route) {
 			this.$mRouter.push({ route });
 		},
-		// 获取产品详情
-		async getProductDetail(row) {
-			await this.$http
-				.get(`${productDetail}`, {
-					id: row.id
-				})
-				.then(async r => {
-					this.productDetail = await r.data;
-					this.productDetail.sku_name = row.sku_name;
-					this.specClass = 'show';
-				});
-		},
-		// 规格弹窗开关
-		toggleSpec(row) {
-			if (parseInt(row.status, 10) === 0) return;
-			if (this.specClass === 'show') {
-				this.specClass = 'hide';
-				if (row.stock === 0) {
-					this.$mHelper.toast('库存不足');
-					return;
-				}
-				if (row.type.toString() === '1') {
-					// 加入购物车
-					this.handleCartItemCreate(row.skuId, row.cartCount);
-				} else if (row.type.toString() === '2') {
-					// 立即购买
-					const list = {};
-					const data = {};
-					data.sku_id = row.skuId;
-					data.num = row.cartCount;
-					if (
-						this.productDetail.point_exchange_type.toString() === '2' ||
-						this.productDetail.point_exchange_type.toString() === '4' ||
-						(this.productDetail.point_exchange_type.toString() === '3' &&
-							this.isPointExchange)
-					) {
-						list.type = 'point_exchange';
-					} else {
-						list.type = 'buy_now';
-					}
-					list.data = JSON.stringify(data);
-					this.navTo(`/pages/order/create/order?data=${JSON.stringify(list)}`);
-				}
-				setTimeout(() => {
-					this.specClass = 'none';
-				}, 250);
-			} else if (this.specClass === 'none') {
-				this.getProductDetail(row);
-			}
-		},
-		stopPrevent() {},
-		hideSpec() {
-			this.specClass = 'hide';
-			setTimeout(() => {
-				this.specClass = 'none';
-			}, 250);
-		},
-	
-	}
+	},
 };
 </script>
